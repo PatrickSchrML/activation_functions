943e40edbf423a07df402ddd3eec0eb60ff0e023
PatrickSchrML <patrick_schramowski@hotmail.de>
2020-05-05 16:18:38 +0200

    fix

--- src/pau_nn_representation/cifar10.py
+++ src/pau_nn_representation/cifar10.py
@@ -62,8 +62,8 @@ def prepare(hparams):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--train_mode', type=str, default='train_representations')
-    parser.add_argument('--classifier', type=str, default='vgg16_bn')
-    parser.add_argument('--classifier_approx', type=str, default='vgg8_bn_pau')
+    parser.add_argument('--classifier', type=str, default='vgg19_bn')
+    parser.add_argument('--classifier_approx', type=str, default='vgg11_bn_pau')
     parser.add_argument('--data_dir', type=str, default='/media/disk2/datasets/pytorch_dataset/cifar10/')
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--batch_size', type=int, default=256)
