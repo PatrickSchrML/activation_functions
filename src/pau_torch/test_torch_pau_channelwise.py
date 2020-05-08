0c79937e8f73761dca4d65ae3d3984e80ec98f18
PatrickSchrML <patrick_schramowski@hotmail.de>
2020-05-08 16:35:45 +0200

    test

--- src/pau_torch/test_torch_pau_channelwise.py
+++ src/pau_torch/test_torch_pau_channelwise.py
@@ -7,13 +7,13 @@ inp = torch.ones([1, 2, 8, 8], dtype=torch.float32)
 pau_conv = PAU_conv(channels=2, cuda=False)
 pau = PAU(cuda=False)
 
-for name, p in pau.named_parameters():
+for name, p in pau_conv.named_parameters():
     if '0.center' in name:
         pass
     if '1.center' in name:
         p.data = torch.ones(p.shape)
 
-for name, p in pau.named_parameters():
+for name, p in pau_conv.named_parameters():
     if '.center' in name:
         print(name, p)
 
